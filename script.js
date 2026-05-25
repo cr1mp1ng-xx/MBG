63445671b32db50e37df5e8a01e1123fc49c71eb
Aliim <aliimathoillah@gmail.com>
2026-05-25 14:28:33 +0700

    Terbaru 2

---
+++ script.js
@@ -0,0 +1,40 @@
+/*LOGIN*/
+function togglePassword() {
+    const password = document.getElementById("password");
+    const eyeIcon = document.querySelector(".right-icon");
+
+    if(password.type === "password"){
+        password.type = "text";
+        eyeIcon.classList.remove("fa-eye");
+        eyeIcon.classList.add("fa-eye-slash");
+    } else {
+        password.type = "password";
+        eyeIcon.classList.remove("fa-eye-slash");
+        eyeIcon.classList.add("fa-eye");
+    }
+}
+
+const inputs = document.querySelectorAll("input");
+    inputs.forEach((input) => {
+
+    input.addEventListener("focus", () => {
+        input.style.border = "1px solid #007a4d";
+        input.style.boxShadow = "0 0 5px rgba(0,122,77,0.3)";
+    });
+
+    input.addEventListener("blur", () => {
+        input.style.border = "1px solid #cfd8d3";
+        input.style.boxShadow = "none";
+    });
+});
+
+const loginButton = document.querySelector(".btn-login");
+    loginButton.addEventListener("click", () => {
+
+    loginButton.innerHTML = "Loading...";
+
+    setTimeout(() => {
+        loginButton.innerHTML =
+        'Masuk ke Dashboard <i class="fa-solid fa-right-to-bracket"></i>';
+    }, 2000);
+});
