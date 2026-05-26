39b2fd3ce2efb63b6992d2a79d650e484e6a9163
Aliim <aliimathoillah@gmail.com>
2026-05-26 13:18:21 +0700

    baru

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-/*LOGIN*/
-const togglePassword =
-document.getElementById('togglePassword');
-
-const password =
-document.getElementById('password');
-
-togglePassword.addEventListener('click', () => {
-
-  const type =
-  password.getAttribute('type') === 'password'
-  ? 'text'
-  : 'password';
-
-  password.setAttribute('type', type);
-
-  togglePassword.classList.toggle('fa-eye');
-
-  togglePassword.classList.toggle('fa-eye-slash');
-
-});
-
 // TRACKING 
 // tombol selesai
 
